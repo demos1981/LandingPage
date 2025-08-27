6ae1ffcaf90b407500b880c0763cfc9dcb32bd88
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2025-08-28 01:30:29 +0300

    add: add new components

---
+++ src/data/dataNavigate.ts
@@ -0,0 +1,6 @@
+export const dataNavigate = [
+  { id: 1, name: "Home", link: "/home" },
+  { id: 2, name: "About", link: "/about" },
+  { id: 3, name: "Services", link: "/services" },
+  { id: 4, name: "Contact", link: "/contact" },
+];
