6a3a23a7c7c6bcb33ab66911e031f8b1cece3b92
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2025-09-02 00:44:14 +0300

    add: add picture and heroes component

---
+++ src/data/dataTrustUsers.ts
@@ -0,0 +1,33 @@
+import Girl1 from "../assets/Girl1.png";
+import Girl2 from "../assets/Girl2.png";
+import Girl3 from "../assets/Girl3.png";
+import Man1 from "../assets/Man1.png";
+import Man2 from "../assets/Man2.png";
+
+export const dataTrustUsers = [
+  {
+    img: Girl1,
+    name: "Каріна",
+    description: "Найкраще що придбала в магазині.",
+  },
+  {
+    img: Girl2,
+    name: "Юлія",
+    description: "Довго вибирала та всеж купила та не жалкую.",
+  },
+  {
+    img: Man1,
+    name: "Сергій",
+    description: "Вибрав для себе та друга. Друг також задоволений.",
+  },
+  {
+    img: Girl3,
+    name: "Софія",
+    description: "Брала мамі та таткові, мабуть ще візьму і для себе.",
+  },
+  {
+    img: Man2,
+    name: "Олександр",
+    description: "Дуже часто в дорозі тож тримаю при собі.",
+  },
+];
