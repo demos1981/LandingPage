cbce2a6b6f5c6d9c2268735d11df2891d9165cbd
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2025-09-04 01:29:31 +0300

    change: change hero section component

---
+++ src/data/dataProduct.ts
@@ -0,0 +1,67 @@
+import photoUmbrella10 from "../assets/photoUmbrella10.jpg";
+import photoUmbrella20 from "../assets/photoUmbrella20.jpg";
+import photoUmbrella30 from "../assets/photoUmbrella30.jpg";
+import photoUmbrella40 from "../assets/photoUmbrella40.jpg";
+
+export const dataProduct = [
+  {
+    id: 1,
+    img: photoUmbrella10,
+    name: "Користувач 1",
+    description: "Колір парасольки 1",
+  },
+  {
+    id: 2,
+    img: photoUmbrella20,
+    name: "Користувач 2",
+    description: "Колір парасольки 2",
+  },
+  {
+    id: 3,
+    img: photoUmbrella30,
+    name: "Парасолька 3",
+    description: "Колір парасольки 3",
+  },
+  {
+    id: 4,
+    img: photoUmbrella40,
+    name: "Парасолька 4",
+    description: "Колір парасольки 4",
+  },
+  {
+    id: 5,
+    img: photoUmbrella10,
+    name: "Парасолька 5",
+    description: "Колір парасольки 5",
+  },
+  {
+    id: 6,
+    img: photoUmbrella20,
+    name: "Парасолька 6",
+    description: "Колір парасольки 6",
+  },
+  {
+    id: 7,
+    img: photoUmbrella30,
+    name: "Парасолька 7",
+    description: "Колір парасольки 7",
+  },
+  {
+    id: 8,
+    img: photoUmbrella40,
+    name: "Парасолька 8",
+    description: "Колір парасольки 8",
+  },
+  {
+    id: 9,
+    img: photoUmbrella10,
+    name: "Парасолька 9",
+    description: "Колір парасольки 9",
+  },
+  {
+    id: 10,
+    img: photoUmbrella20,
+    name: "Парасолька 10",
+    description: "Колір парасольки 10",
+  },
+];
