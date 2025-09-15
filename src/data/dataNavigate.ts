86af039e906aaca8bf59ff3548db999de887649d
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2025-09-16 00:12:41 +0300

    add: add new photo to asset and component

--- src/data/dataNavigate.ts
+++ src/data/dataNavigate.ts
@@ -1,6 +1,6 @@
 export const dataNavigate = [
   { id: 1, name: "Home", link: "/" },
-  { id: 2, name: "About", link: "/about" },
-  { id: 3, name: "Services", link: "/services" },
+  { id: 2, name: "Products", link: "/products" },
+  { id: 3, name: "Features", link: "/features" },
   { id: 4, name: "Contact", link: "/contact" },
 ];
--- src/data/dataProduct.ts
+++ src/data/dataProduct.ts
@@ -1,67 +1,122 @@
-import photoUmbrella10 from "../assets/photoUmbrella10.jpg";
-import photoUmbrella20 from "../assets/photoUmbrella20.jpg";
-import photoUmbrella30 from "../assets/photoUmbrella30.jpg";
-import photoUmbrella40 from "../assets/photoUmbrella40.jpg";
+import photoUmbrella1 from "../assets/izumit1.jpg";
+import photoUmbrella2 from "../assets/izumit2.jpg";
+import photoUmbrella3 from "../assets/izumit3.jpg";
+import photoUmbrella4 from "../assets/izumit4.jpg";
+import photoUmbrella5 from "../assets/izumit5.jpg";
+import photoUmbrella6 from "../assets/izumit6.jpg";
+import photoUmbrella7 from "../assets/izumit7.jpg";
+import photoUmbrella8 from "../assets/izumit8.jpg";
+import photoUmbrella9 from "../assets/izumit9.jpg";
+import photoUmbrella10 from "../assets/izumit10.jpg";
+import photoUmbrella11 from "../assets/izumit11.jpg";
+import photoUmbrella12 from "../assets/izumit12.jpg";
+import photoUmbrella13 from "../assets/izumit13.jpg";
+import photoUmbrella14 from "../assets/izumit14.jpg";
 
 export const dataProduct = [
   {
     id: 1,
-    img: photoUmbrella10,
-    name: "Користувач 1",
-    description: "Колір парасольки 1",
+    img: photoUmbrella1,
+    name: "Парасолька Water Town",
+    description:
+      "Парасолька середнього розміру, чорного кольору, чохол на блискавці, дерев'яне руків'я, автоматична",
+    price: 100,
+    article: "101801",
   },
   {
     id: 2,
-    img: photoUmbrella20,
-    name: "Парасолька 2",
-    description: "Колір парасольки 2",
+    img: photoUmbrella2,
+    name: "Парасолька Mario",
+    description:
+      "Парасолька середнього розміру, двоколірна червоно-біла, пластикове руків'я, автоматична",
+    price: 100,
+    article: "160140",
   },
   {
     id: 3,
-    img: photoUmbrella30,
-    name: "Парасолька 3",
-    description: "Колір парасольки 3",
+    img: photoUmbrella3,
+    name: "Парасолька Mario",
+    description:
+      "Парасолька середнього розміру, двоколірна синьо-біла, пластикове руків'я, автоматична",
+    price: 100,
+    article: "160141",
   },
   {
     id: 4,
-    img: photoUmbrella40,
-    name: "Парасолька 4",
-    description: "Колір парасольки 4",
+    img: photoUmbrella4,
+    name: "Парасолька Mario",
+    description:
+      "Парасолька середнього розміру, двоколірна фіолетово-біла, пластикове руків'я, автоматична",
+    price: 100,
+    article: "160142",
   },
   {
     id: 5,
-    img: photoUmbrella10,
-    name: "Парасолька 5",
-    description: "Колір парасольки 5",
+    img: photoUmbrella5,
+    name: "Парасолька Mario",
+    description:
+      "Парасолька середнього розміру, двоколірна рожево-біла, пластикове руків'я, автоматична",
+    price: 100,
+    article: "160139",
   },
   {
     id: 6,
-    img: photoUmbrella20,
-    name: "Парасолька 6",
-    description: "Колір парасольки 6",
+    img: photoUmbrella6,
+    name: "Парасолька Mario",
+    description:
+      "Парасолька середнього розміру, двоколірна помаранчево-біла, пластикове руків'я, автоматична",
+    price: 100,
+    article: "160143",
   },
   {
     id: 7,
-    img: photoUmbrella30,
-    name: "Парасолька 7",
-    description: "Колір парасольки 7",
+    img: photoUmbrella7,
+    name: "Парасолька Mario",
+    description:
+      "Парасолька середнього розміру, двоколірна сіро-біла, пластикове руків'я, автоматична",
+    price: 100,
+    article: "160138",
   },
   {
     id: 8,
-    img: photoUmbrella40,
+    img: photoUmbrella8,
     name: "Парасолька 8",
     description: "Колір парасольки 8",
   },
   {
     id: 9,
-    img: photoUmbrella10,
+    img: photoUmbrella9,
     name: "Парасолька 9",
     description: "Колір парасольки 9",
   },
   {
     id: 10,
-    img: photoUmbrella20,
+    img: photoUmbrella10,
     name: "Парасолька 10",
     description: "Колір парасольки 10",
+  },
+  {
+    id: 11,
+    img: photoUmbrella11,
+    name: "Парасолька 11",
+    description: "Колір парасольки 11",
+  },
+  {
+    id: 12,
+    img: photoUmbrella12,
+    name: "Парасолька 12",
+    description: "Колір парасольки 12",
+  },
+  {
+    id: 13,
+    img: photoUmbrella13,
+    name: "Парасолька 13",
+    description: "Колір парасольки 13",
+  },
+  {
+    id: 14,
+    img: photoUmbrella14,
+    name: "Парасолька 14",
+    description: "Колір парасольки 14",
   },
 ];
